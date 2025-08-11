4dcdd4a6ae1bde98e005abd9a961fc0b28fca75a
samwelnella <danielsamwel@gmail.com>
2025-08-10 21:48:00 -0400

    Remove all build files from git

--- app/build/generated/source/kapt/debug/com/romm/android/workers/FirmwareDownloadWorker_AssistedFactory.java
+++
@@ -1,8 +0,0 @@
-package com.romm.android.workers;
-
-import androidx.hilt.work.WorkerAssistedFactory;
-import dagger.assisted.AssistedFactory;
-
-@AssistedFactory
-public interface FirmwareDownloadWorker_AssistedFactory extends WorkerAssistedFactory<FirmwareDownloadWorker> {
-}
--- app/build/generated/source/kapt/debug/com/romm/android/workers/GameDownloadWorker_AssistedFactory.java
+++
@@ -1,8 +0,0 @@
-package com.romm.android.workers;
-
-import androidx.hilt.work.WorkerAssistedFactory;
-import dagger.assisted.AssistedFactory;
-
-@AssistedFactory
-public interface GameDownloadWorker_AssistedFactory extends WorkerAssistedFactory<GameDownloadWorker> {
-}
